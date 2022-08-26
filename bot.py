76d5b2491e72cc0dbd25a77f95f87cd26c009946
Andrew Lee <C22Andrew.Lee@usafa.edu>
2022-08-25 21:10:34 -0600

    expected move formula added

--- bot.py
+++ bot.py
@@ -5,12 +5,11 @@ import json
 
 from datetime import datetime
 
-
-STOCK = 'GOOG'
+STOCK = 'SPY'
 
 def main() -> None:
     print()
-    print("-------- Starting bot for the day ------------"), print()
+    print("-------- Starting bot for the day --------"), print()
 
     if pre_market_open():
         print("Pre - NYSE is open")
@@ -25,21 +24,42 @@ def main() -> None:
     now = datetime.now()
     position = get_position(client)
 
-    print(now), print()
+    print(now)
 
 
-        # get current position
-    print('HAS POSITION: ' + str(position))
+    # get current position
+    #print('HAS POSITION: ' + str(position))
 
-        # get current price
+    # get current price
     price = getprice(client, STOCK)
 
-    print("Current price " + str(price))
+    print("Current price " + str(price)), print()
 
+    print("------ Call Options ------")
+    print("Range / days to expiration is: " + str(DAYSOUT))
     callOption = call_option_chain(client, STOCK)
-    putOption = put_option_chain(client, STOCK)
+    callIV = getCallIV(callOption)
+    print("Call IV Based On Freq = " + str(callIV))
+    callexpected = expectedRange(callIV, DAYSOUT, price)
+    print("Expected range +/- : " + str(callexpected))
+    print("Expected stock range: " + str(callexpected + price) + " to " + str(price - callexpected))
 
-    print("Call IV = " + str(getCallIV(callOption)))
+    print()
+    print("------ Put Options ------")
+    print("Range / days to expiration is: " + str(DAYSOUT))
+    putOption = put_option_chain(client, STOCK)
+    putIV = getPutIV(putOption)
+    print("Put IV based on Freq = " + str(putIV))
+    putexpected = expectedRange(putIV, DAYSOUT, price)
+    print("Expected range +/- : " + str(putexpected))
+    print("Expected stock range: " + str(putexpected + price) + " to " + str(price - putexpected))
+
+
+    #putOption = put_option_chain(client, STOCK)
+
+
+    #print("Call IV = " + str(getCallIV(callOption))), print()
+    #print("Put IV = " + str(getPutIV(putOption)))
 
 
 if __name__=="__main__":
--- getData.py
+++ getData.py
@@ -3,6 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from auth_params import ACCT, API, CALLBACK
 from selenium import webdriver
 
+import math
 import numpy as np
 
 from datetime import date, timedelta
@@ -12,6 +13,7 @@ import json
 CHAINSIZE = 30
 DAYSOUT = 2
 
+#used to authenticate client
 def authenticate():
     token_path = 'aaah.json'
     try:
@@ -22,6 +24,7 @@ def authenticate():
                 driver, API, CALLBACK, token_path)
     return client
 
+#returns last price of ticker
 def getprice(client, ticker: str):
     r = client.get_quote(ticker)
     assert r.status_code == 200, r.raise_for_status()
@@ -30,6 +33,7 @@ def getprice(client, ticker: str):
     price = y[ticker]["lastPrice"]
     return price
 
+#returns positions
 def get_position(client):
     r = client.get_account(ACCT, fields=client.Account.Fields.POSITIONS)
     assert r.status_code == 200, r.raise_for_status()
@@ -41,6 +45,7 @@ def get_position(client):
     else:
         return False
 
+#get the call options chain
 def call_option_chain(client, ticker: str):
     expDate = date.today() + timedelta(DAYSOUT)
     #strike_range=client.Options.StrikeRange.NEAR_THE_MONEY, if you want NTM
@@ -59,6 +64,7 @@ def call_option_chain(client, ticker: str):
         json.dump(out, f, indent=2)
     return out
 
+#gets put options chain
 def put_option_chain(client, ticker: str):
     expDate = date.today() + timedelta(DAYSOUT)
 
@@ -77,13 +83,16 @@ def put_option_chain(client, ticker: str):
         json.dump(out, f, indent=2)
     return out
 
+#returns IV for call chain
 def getCallIV(call) -> float:
     callStrikes = (call['callExpDateMap'][list(call['callExpDateMap'].keys())[0]].keys())
-    #print(callStrikes)
     listCallStrikes = list(callStrikes)
     listCallVol =[]
     listCalltotVol = []
     listCallopIn = []
+
+    #formatting for option chain
+    #print("-----------Call option chain-----------")
 
     for x in listCallStrikes:
         callvol = call['callExpDateMap'][list(call['callExpDateMap'].keys())[0]][x][0]['volatility']
@@ -104,7 +113,48 @@ def getCallIV(call) -> float:
 
     newList = replaceNan(getAvg(listCallVol), listCallVol)
 
+    print('Avg IV for ' + str(CHAINSIZE) + ': ' + str(getAvg(listCallVol)))
+
     return round(((newList[indexOne] + newList[indexTwo]) / 2), 3)
+
+
+def getPutIV(put) -> float:
+    putStrikes = (put['putExpDateMap'][list(put['putExpDateMap'].keys())[0]].keys())
+    listPutStrikes = list(putStrikes)
+    listPutVol =[]
+    listPuttotVol = []
+    listPutopIn = []
+
+    #formatting for option chain
+    #print("-----------put option chain-----------")
+
+    for x in listPutStrikes:
+        putvol = put['putExpDateMap'][list(put['putExpDateMap'].keys())[0]][x][0]['volatility']
+        puttotVol = put['putExpDateMap'][list(put['putExpDateMap'].keys())[0]][x][0]['totalVolume']
+        putopIn = put['putExpDateMap'][list(put['putExpDateMap'].keys())[0]][x][0]['openInterest']
+
+        #code to pring the option chain if you want to see it
+        #print(json.dumps(put['putExpDateMap'][list(put['putExpDateMap'].keys())[0]][x][0]['description'],indent = 4) + " " +
+        #    str(putvol) + " " + str(puttotVol) + " " + str(putopIn)
+        #)
+
+        listPutVol.append(putvol)
+        listPuttotVol.append(puttotVol)
+        listPutopIn.append(putopIn)
+
+    indexOne = np.argmax(np.array(listPuttotVol))
+    indexTwo = np.argmax(np.array(listPutopIn))
+
+    newList = replaceNan(getAvg(listPutVol), listPutVol)
+
+    print('Avg IV for ' + str(CHAINSIZE) + ': ' + str(getAvg(listPutVol)))
+
+    return round(((newList[indexOne] + newList[indexTwo]) / 2), 3)
+
+#expected move formula using IV
+def expectedRange(volatility, daysToExpiration, stockPrice) -> float:
+    sq = math.sqrt(daysToExpiration / 365)
+    return round((sq * (volatility/100) * stockPrice), 2)
 
 #returns the avg but should remove the Nan's
 def getAvg(l):
